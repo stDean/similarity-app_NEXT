84c45439223195dbccf3b3ca3c0a007e88f91eb6
Toba-dean <tobaogundimu@gmail.com>
2023-06-10 00:12:06 +0100

    added the prisma schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -32,4 +32,38 @@ export const authOptions: NextAuthOptions = {
       clientSecret: getGoogleCredentials().clientSecret,
     }),
   ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!dbUser) {
+        token.id = user!.id
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+    redirect() {
+      return '/dashboard'
+    },
+  }
 }
