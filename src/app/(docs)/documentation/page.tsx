3691a3ecba15574087056e20388723f293159aed
Toba-dean <tobaogundimu@gmail.com>
2023-06-09 18:32:56 +0100

    started the docs page

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import "simplebar-react/dist/simplebar.min.css";
+
+import { Paragraph, LargeHeading, DocumentationTabs } from "@/components";
+
+export const metadata: Metadata = {
+  title: 'Similarity API | Documentation',
+  description: 'Free & open-source text similarity API',
+}
+
+const page = () => {
+  return (
+    <div className='container max-w-7xl mx-auto mt-12'>
+      <div className='flex flex-col items-center gap-6'>
+        <LargeHeading>Making a request</LargeHeading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
+      </div>
+    </div>
+  );
+}
+
+export default page;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as NavBar } from "./NavBar";
 export { default as Providers } from "./Providers";
 export { default as Paragraph } from "./ui/Paragraph";
 export { default as LargeHeading } from "./ui/LargeHeading";
+export { default as DocumentationTabs } from "./DocumentationTabs";
